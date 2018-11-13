cd6a5657cbcfc1192d8f283de857f9121551153e
dldxz <335034654@qq.com>
2018-11-13 12:06:47 +0800

    init

---
+++ src/login.php
@@ -0,0 +1,21 @@
+<?php
+include('sqli.php')
+$dbuser = "root";
+$dbpass = "hzb123456";
+$dbname = "library";
+$host = "localhost";
+
+@error_reporting(0);
+@$con = mysqli_connect($host,$dbuser,$dbpass,$dbname);
+if (!$con)
+{
+    echo "Failed to connect to MySQL: " . mysqli_error();
+}
+$username = dowith_sql($_POST['username']);
+$passwd = dowith_sql($_POST['password']);
+$sql = "SELECT * FROM user WHERE username=('$username') AND password=('$passwd') LIMIT 0,1";
+$result = mysqli_query($sql);
+$row = mysqli_fetch_array($result);
+$json_data = json_encode($row);
+echo "$json_data";
+ ?>
---
+++ src/sqli.php
@@ -0,0 +1,27 @@
+<?php
+function dowith_sql($str)
+{
+   $str = str_replace("and","",$str);
+   $str = str_replace("execute","",$str);
+   $str = str_replace("update","",$str);
+   $str = str_replace("count","",$str);
+   $str = str_replace("chr","",$str);
+   $str = str_replace("mid","",$str);
+   $str = str_replace("master","",$str);
+   $str = str_replace("truncate","",$str);
+   $str = str_replace("char","",$str);
+   $str = str_replace("declare","",$str);
+   $str = str_replace("select","",$str);
+   $str = str_replace("create","",$str);
+   $str = str_replace("delete","",$str);
+   $str = str_replace("insert","",$str);
+   $str = str_replace("'","",$str);
+   $str = str_replace('"',"",$str);
+   $str = str_replace(" ","",$str);
+   $str = str_replace("or","",$str);
+   $str = str_replace("=","",$str);
+   $str = str_replace("%20","",$str);
+   $str = str_replace(")","",$str);
+   return $str;
+}
+ ?>
