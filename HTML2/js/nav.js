7dc091f6d785153363c688392cfce4623d1022ae
loranloranvl <1194452940@qq.com>
2018-12-17 21:41:40 +0800

    login

--- HTML2/js/nav.js
+++ HTML2/js/nav.js
@@ -1,5 +1,10 @@
+isAdmin = 1
+var nav
 $(document).ready(function() {
-    var nav = new Vue({
+    if (location.href.search('login.html') != -1) {
+        return
+    }
+    nav = new Vue({
         el: '#nav',
         data: {
             isAdmin: isAdmin,
@@ -7,28 +12,54 @@ $(document).ready(function() {
             stuNav: [
                 {
                     name: '图书查询',
-                    link: 'query.html'
+                    link: 'query.html' // db: book r
                 },
                 {
                     name: '借阅记录',
-                    link: 'history.html'
+                    link: 'history.html' // db: borrow_record r | arrear_record r
+                },
+                {
+                    name: '图书预约',
+                    link: 'reserve.html' // db: reserve_record ru
                 }
             ],
             adminNav: [
                 {
-                    name: '录入图书',
-                    link: 'newbook.html'
+                    name: '图书查询',
+                    link: 'query.html' // db: book r
                 },
                 {
                     name: '借书还书',
-                    link: 'process.html'
+                    link: 'process.html' // db: borrow_record ru
                 },
                 {
-                    name: '借书证办理',
-                    link: 'card.html'
+                    name: '图书预约',
+                    link: 'reserve.html' // db: reserve_record r
+                },
+                {
+                    name: '录入图书',
+                    link: 'newbook.html' // db: inbound_record ru
+                },
+                {
+                    name: '逾期管理',
+                    link: 'arrear.html' // db: arrear_record ru
                 }
+            ],
+            sentences: [
+                '读过一本好书，像交了一个益友。——臧克家 ',
+                '读书破万卷，下笔如有神。——杜甫',
+                '不读书的人，思想就会停止。——狄德罗',
+                '凡读无益之书，皆是玩物丧志。——（清）王豫',
+                '读书对于智慧，就象体操对于身体一样。——（英国）谚语',
+                '读书不知味，不如束高阁；蠢鱼尔何如，终日食糟粕。——（清）袁牧',
+                '鸟欲高飞先振翅，人求上进先读书。——李苦禅',
+                '没有比读书更好的娱乐更持久的满足了。——（英国）谚语',
+                '进学致和，行方思远。——字严',
+                '壮士腰间三尺剑；男儿腹中五车书。——《对联集锦》',
+                '学问对人们要求最大的紧张和最大的热情。——巴甫洛夫',
+                '学习并不等于就是摹仿某些东西，而是掌握技巧和方法。——高尔基'
             ]
-        }，
+        },
         methods: {
             isCurrent: function(link) {
                 splitted = location.href.split('/')
